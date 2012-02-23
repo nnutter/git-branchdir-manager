529448dc334d73e486c1a56d950a4447513dde1a
Nathan Nutter <iam@nnutter.com>
2012-02-23 10:59:17 -0600

    v2

--- git-branchdir-manager.sh
+++ git-branchdir-manager.sh
@@ -3,35 +3,35 @@ complete -F _gb_complete b
 complete -F _gb_complete git-branch
 
 function _gb_env {
-    if [ -z "$GB_BASE_DIR" ]; then
-        GB_BASE_DIR="$HOME/git"
-    fi
-    if [ -z "$GB_DEV_SRC_BRANCH" ]; then
-        GB_DEV_SRC_BRANCH="origin/master"
-    fi
+    [ -z "$GB_BASE_DIR" ]        && GB_BASE_DIR="$HOME/git"
+    [ -z "$GB_DEV_BRANCH" ]      && GB_DEV_BRANCH="master"
+    [ -z "$GB_DEV_REMOTE" ]      && GB_DEV_REMOTE="origin"
+    [ -z "$GB_MASTER_DIR_NAME" ] && GB_MASTER_DIR_NAME=".gb_master"
+    [ -z "$GB_MASTER_BRANCH" ]   && GB_MASTER_BRANCH="gb_master"
+    [ -z "$GB_DEV_REMOTE_REF" ]  && GB_DEV_REMOTE_REF="$GB_DEV_REMOTE/$GB_DEV_BRANCH"
 }
 
 function _gb_help {
     echo "Usage:"
     echo "  git-branch <repo_name> init <repo_url>"
-    echo "  git-branch <repo_name> <new_branch_name> new"
-    echo "  git-branch <repo_name> <exiting_branch_name>"
-    echo "  git-branch <repo_name> <exiting_branch_name> merge"
-    echo "  git-branch <repo_name> <exiting_branch_name> rm"
-    echo "  git-branch <repo_name> <exiting_branch_name> lib"
+    echo "  git-branch <repo_name> <branch_name> start"
+    echo "  git-branch <repo_name> <branch_name>"
+    echo "  git-branch <repo_name> <branch_name> finish"
+    echo "  git-branch <repo_name> <branch_name> rm"
+    echo "  git-branch <repo_name> <branch_name> lib"
 }
 
 function _gb_repos {
     _gb_env
     if [ -d "$GB_BASE_DIR" ]; then
-        find "$GB_BASE_DIR" -mindepth 1 -maxdepth 2 -type d -name master | sed -e "s|$GB_BASE_DIR/||" -e "s|/master||"
+        find "$GB_BASE_DIR" -mindepth 1 -maxdepth 2 -type d -name $GB_MASTER_DIR_NAME | sed -e "s|$GB_BASE_DIR/||" -e "s|/$GB_MASTER_DIR_NAME||"
     fi
 }
 
 function _gb_repo_master_dir {
     _gb_env
     local GB_REPO="$1"
-    local GB_BRANCH="master"
+    local GB_BRANCH="$GB_MASTER_DIR_NAME"
     local GB_BRANCH_DIR="$GB_BASE_DIR/$GB_REPO/$GB_BRANCH"
     echo "$GB_BRANCH_DIR"
 }
@@ -41,7 +41,7 @@ function _gb_branches {
     local GB_REPO=$1
     local GB_REPO_DIR="$GB_BASE_DIR/$GB_REPO"
     if [ -d "$GB_REPO_DIR" ]; then
-        find "$GB_REPO_DIR" -mindepth 1 -maxdepth 2 -type d -name .git | sed -e "s|$GB_REPO_DIR/||" -e "s|/.git||"
+        find "$GB_REPO_DIR" -mindepth 1 -maxdepth 2 -type d -name .git | sed -e "s|$GB_REPO_DIR/||" -e "s|/.git||" | grep -v "$GB_MASTER_DIR_NAME"
     fi
 }
 
@@ -60,13 +60,8 @@ function _gb_lib_path {
     local GB_BRANCH_DIR="$GB_BASE_DIR/$GB_REPO/$GB_BRANCH"
     local GB_LIB_DIR
 
-    if [ -d "$GB_BRANCH_DIR/lib/perl" ]; then
-        GB_LIB_DIR="$GB_BRANCH_DIR/lib/perl"
-    else
-        if [ -d "$GB_BRANCH_DIR/lib" ]; then
-            GB_LIB_DIR="$GB_BRANCH_DIR/lib"
-        fi
-    fi
+    [ -d "$GB_BRANCH_DIR/lib" ] && GB_LIB_DIR="$GB_BRANCH_DIR/lib"
+    [ -d "$GB_BRANCH_DIR/lib/perl" ] && GB_LIB_DIR="$GB_BRANCH_DIR/lib/perl"
     echo "$GB_LIB_DIR"
 }
 
@@ -75,35 +70,41 @@ function _gb_cd_lib_dir {
     if [ -d "$GB_BRANCH_DIR/lib/perl/Genome" ]; then
         cd "$GB_BRANCH_DIR/lib/perl/Genome"
     else
-        if [ -d "$GB_BRANCH_DIR/lib" ]; then
-            cd "$GB_BRANCH_DIR/lib"
-        fi
+        [ -d "$GB_BRANCH_DIR/lib" ] && cd "$GB_BRANCH_DIR/lib"
     fi
 }
 
 function _gb_cd_branch {
     _gb_env
-    local GB_REPO="$1"
-    local GB_BRANCH="$2"
+    GB_REPO="$1"
+    GB_BRANCH="$2"
     local GB_BRANCH_DIR="$GB_BASE_DIR/$GB_REPO/$GB_BRANCH"
-    local GB_DO_CD_BRANCH=1
 
     if [ ! -d "$GB_BRANCH_DIR" ]; then
-        GB_DO_CD_BRANCH=0
         echo "ERROR: Branch directory does not exist ($GB_BRANCH_DIR)."
         echo -n "Do you wish to create the branch (y/n)? "
         local response
         read -n 1 response && echo
         if [ "$response" == "y" ]; then
-            _gb_new_branch "$GB_REPO" "$GB_BRANCH"
+            _gb_start_branch "$GB_REPO" "$GB_BRANCH"
+        else
+            return 255
         fi
     fi
 
-    if (( $GB_DO_CD_BRANCH )); then
-        cd "$GB_BRANCH_DIR"
-    fi
-
+    cd "$GB_BRANCH_DIR"
     _gb_cd_lib_dir "$GB_BRANCH_DIR"
+}
+
+function _git_tracking_ref {
+    _gb_env
+    local GB_UPSTREAM_BRANCH=$(git config --get branch.$GB_BRANCH.merge | sed 's/^refs\/heads\///')
+    local GB_UPSTREAM_REMOTE=$(git config --get branch.$GB_BRANCH.remote)
+    if [ -z "$GB_UPSTREAM_BRANCH" ] || [ -z "$GB_UPSTREAM_REMOTE" ]; then
+        return 255
+    else
+        echo "$GB_UPSTREAM_REMOTE/$GB_UPSTREAM_BRANCH"
+    fi
 }
 
 function _gb_rm_branch {
@@ -112,65 +113,50 @@ function _gb_rm_branch {
     local GB_REPO="$1"
     local GB_BRANCH="$2"
     local GB_BRANCH_DIR="$GB_BASE_DIR/$GB_REPO/$GB_BRANCH"
-    local GB_DO_RM_BRANCH=1
 
     if [ ! -d "$GB_BRANCH_DIR" ]; then
-        GB_DO_RM_BRANCH=0
         echo "ERROR: Branch directory does not exist ($GB_BRANCH_DIR)."
+        return 255
     fi
 
     PRIOR_DIR=$(pwd)
-    if [ -d "$GB_BRANCH_DIR" ]; then
-        cd "$GB_BRANCH_DIR"
-    fi
+    [[ -d "$GB_BRANCH_DIR" ]] && cd "$GB_BRANCH_DIR"
 
-    local GB_UPSTREAM_BRANCH=$(git config --get branch.$GB_BRANCH.merge | sed 's/^refs\/heads\///')
-    local GB_UPSTREAM_REMOTE=$(git config --get branch.$GB_BRANCH.remote)
-    if [ -z "$GB_UPSTREAM_BRANCH" ] || [ -z "$GB_UPSTREAM_REMOTE" ]; then
-        GB_DO_RM_BRANCH=0
-        echo "ERROR: Unable to determine upstream branch."
-        echo "git config --get branch.$GB_BRANCH.merge | sed 's/^refs\/heads\///'"
-        echo "git config --get branch.$GB_BRANCH.remote"
-    fi
+    local TRACKING_REF=$(_git_tracking_ref "$GB_BRANCH")
+    [[ -z "$TRACKING_REF" ]] && return 255
 
-    if (( $GB_DO_RM_BRANCH )); then
-        if (( $(git log --oneline $GB_UPSTREAM_REMOTE/$GB_UPSTREAM_BRANCH..HEAD | wc -l) )); then
-            echo "ERROR: Unpushed changes in repo:"
-            git log --oneline $GB_UPSTREAM_REMOTE/$GB_UPSTREAM_BRANCH..$GB_BRANCH | cat
-            echo -n "Do you wish to remove this branch (y/n)? "
-            local response
-            read -n 1 response && echo
-            if [ "$response" != "y" ]; then
-                GB_DO_RM_BRANCH=0
-            fi
+    if (( $(git log --oneline $TRACKING_REF..HEAD | wc -l) )); then
+        echo "ERROR: Unpushed changes in repo:"
+        git log --oneline $TRACKING_REF..$GB_BRANCH | cat
+        echo -n "Do you wish to remove this branch (y/n)? "
+        local response
+        read -n 1 response && echo
+        if [ "$response" != "y" ]; then
+            return 255;
         fi
     fi
 
-    if (( $GB_DO_RM_BRANCH )); then
-        if (( $(git status -s | wc -l) )); then
-            echo "ERROR: Uncommitted changes in repo:"
-            git status -s
-            echo -n "Do you wish to remove this branch (y/n)? "
-            local response
-            read -n 1 response && echo
-            if [ "$response" != "y" ]; then
-                GB_DO_RM_BRANCH=0
-            fi
+    if (( $(git status -s | wc -l) )); then
+        echo "ERROR: Uncommitted changes in repo:"
+        git status -s
+        echo -n "Do you wish to remove this branch (y/n)? "
+        local response
+        read -n 1 response && echo
+        if [ "$response" != "y" ]; then
+            return 255;
         fi
     fi
 
-    if (( $GB_DO_RM_BRANCH )); then
-        if (( $(git branch | grep -P "\b$GB_BRANCH\b" | wc -l) )); then
-            if (( $(git branch | grep -P "\* $GB_BRANCH\b" | wc -l) )); then
-                git checkout master
-            fi
-            git branch -D "$GB_BRANCH"
+    if (( $(git branch | grep -P "\b$GB_BRANCH\b" | wc -l) )); then
+        if (( $(git branch | grep -P "\* $GB_BRANCH\b" | wc -l) )); then
+            git checkout -q "$GB_MASTER_BRANCH"
         fi
-
-        echo "Removing '$GB_BRANCH_DIR'..."
-        sleep 1
-        rm -rf "$GB_BRANCH_DIR"
+        git branch -D "$GB_BRANCH"
     fi
+
+    echo "Removing '$GB_BRANCH_DIR'..."
+    sleep 1
+    rm -rf "$GB_BRANCH_DIR"
 
     if [ -d "$PRIOR_DIR" ]; then
         cd $PRIOR_DIR
@@ -179,49 +165,90 @@ function _gb_rm_branch {
     fi
 }
 
-function _gb_new_branch {
+function _gb_new_workdir_path {
+    local GB_GIT_NEW_WORKDIR
+    if which git-new-workdir 1> /dev/null 2> /dev/null; then
+        GB_GIT_NEW_WORKDIR=$(which git-new-workdir)
+    else
+        local GB_TMP_GIT_NEW_WORKDIR="/tmp/git-new-workdir"
+        echo "WARNING: git-new-workdir not installed, downloading to $GB_TMP_GIT_NEW_WORKDIR." 1>&2
+        local GB_GIT_NEW_WORKDIR_URL="https://raw.github.com/gitster/git/master/contrib/workdir/git-new-workdir"
+        curl -s -o "$GB_TMP_GIT_NEW_WORKDIR" "$GB_GIT_NEW_WORKDIR_URL"
+        GB_GIT_NEW_WORKDIR="/bin/sh $GB_TMP_GIT_NEW_WORKDIR"
+    fi
+    echo "$GB_GIT_NEW_WORKDIR"
+}
+
+function _gb_refresh_master {
+    _gb_env
+    if [ "$(_git_current_branch)" != "$GB_MASTER_BRANCH" ]; then
+        git checkout "$GB_MASTER_BRANCH"
+    fi
+    git fetch -q
+    git merge -q "$GB_DEV_REMOTE_REF"
+    git reset -q --hard "$GB_DEV_REMOTE_REF"
+    git clean -qxdf
+}
+
+function _gb_start_branch {
     _gb_env
     local GB_REPO="$1"
     local GB_BRANCH="$2"
-    local GB_MASTER_DIR="$GB_BASE_DIR/$GB_REPO/master"
+
+    local GB_MASTER_DIR="$GB_BASE_DIR/$GB_REPO/$GB_MASTER_DIR_NAME"
     local GB_BRANCH_DIR="$GB_BASE_DIR/$GB_REPO/$GB_BRANCH"
-    local GB_DO_NEW_BRANCH=1
+
     if [ ! -d "$GB_MASTER_DIR" ]; then
-        GB_DO_NEW_BRANCH=0
-        echo "ERROR: Master directory does not exist ($GB_MASTER_DIR)."
+        echo "ERROR: git-branch master directory does not exist ($GB_MASTER_DIR)."
+        return 255
     fi
+
     if [ -d "$GB_BRANCH_DIR" ]; then
-        GB_DO_NEW_BRANCH=0
         echo "ERROR: Branch directory already exists ($GB_BRANCH_DIR)."
+        return 255
     fi
-    if (( $GB_DO_NEW_BRANCH )); then
-        local GB_GIT_NEW_WORKDIR
-        if (( $(which git-new-workdir | wc -l) )); then
-            GB_GIT_NEW_WORKDIR=$(which git-new-workdir)
+
+    local GB_GIT_NEW_WORKDIR=$(_gb_new_workdir_path)
+
+    cd "$GB_MASTER_DIR"
+    _gb_refresh_master
+    $GB_GIT_NEW_WORKDIR "$GB_MASTER_DIR" "$GB_BRANCH_DIR"
+    cd "$GB_BRANCH_DIR"
+    if _git_branch_exists "$GB_BRANCH"; then
+        git checkout -q "$GB_BRANCH"
+        local TRACKING_REF=$(_git_tracking_ref "$GB_BRANCH")
+        if [ -n "$TRACKING_REF" ]; then
+            git reset --hard "$TRACKING_REF"
         else
-            local GB_TMP_GIT_NEW_WORKDIR="/tmp/git-new-workdir"
-            echo "WARNING: git-new-workdir not installed, downloading to $GB_TMP_GIT_NEW_WORKDIR"
-            local GB_GIT_NEW_WORKDIR_URL="https://raw.github.com/gitster/git/master/contrib/workdir/git-new-workdir"
-            curl -s -o "$GB_TMP_GIT_NEW_WORKDIR" "$GB_GIT_NEW_WORKDIR_URL"
-            GB_GIT_NEW_WORKDIR="/bin/sh $GB_TMP_GIT_NEW_WORKDIR"
+            git branch --set-upstream "$GB_BRANCH" "$GB_DEV_REMOTE_REF"
+            git rebase "$GB_DEV_REMOTE_REF"
         fi
-        cd $GB_MASTER_DIR
-        git pull --ff-only || git pull --rebase
-        $GB_GIT_NEW_WORKDIR $GB_MASTER_DIR $GB_BRANCH_DIR
-        cd $GB_BRANCH_DIR
-        git checkout -b $GB_BRANCH --track "$GB_DEV_SRC_BRANCH"
-        git merge master
+    else
+        git checkout -q -b "$GB_BRANCH" -t "$GB_DEV_REMOTE_REF"
     fi
+
     _gb_cd_lib_dir "$GB_BRANCH_DIR"
 }
 
-function _gb_merge_branch {
+function _gb_finish_branch {
     _gb_env
     local GB_REPO="$1"
     local GB_BRANCH="$2"
-    _gb_cd_branch "$GB_REPO" "master"
-    git pull --ff-only || git pull --rebase
-    git merge --no-ff "$GB_BRANCH"
+
+    _gb_cd_branch "$GB_REPO" "$GB_BRANCH"
+    local TRACKING_REF=$(_git_tracking_ref "$GB_BRANCH")
+    [[ -z "$TRACKING_REF" ]] && return 255
+
+    git fetch -q || return 255
+    git rebase -q "$TRACKING_REF" || return 255
+
+    git checkout -q "$GB_MASTER_BRANCH" || return 255
+    _gb_refresh_master || return 255
+    git merge -q --no-ff "$GB_BRANCH" || return 255
+    git push -q origin $GB_MASTER_BRANCH:$GB_DEV_BRANCH || return 255
+
+    git checkout -q "$GB_BRANCH" || return 255
+    git merge -q "$GB_MASTER_BRANCH" || return 255
 }
 
 function _gb_init_repo {
@@ -229,21 +256,23 @@ function _gb_init_repo {
     local GB_REPO="$1"
     local GB_REPO_URL="$2"
     local GB_REPO_BASE_DIR="$GB_BASE_DIR/$GB_REPO"
-    local GB_REPO_MASTER_DIR="$GB_REPO_BASE_DIR/master"
-    local GB_DO_INIT=1
+    local GB_REPO_MASTER_DIR="$GB_REPO_BASE_DIR/$GB_MASTER_DIR_NAME"
+
     if [ -z "$GB_REPO_URL" ]; then
         _gb_help
-        GB_DO_INIT=0
+        return 255
     fi
+
     if [ -d "$GB_REPO_MASTER_DIR" ]; then
         echo "ERROR: Repo master dir already exists ($GB_REPO_MASTER_DIR)."
-        GB_DO_INIT=0
+        return 255
     fi
-    if (( $GB_DO_INIT )); then
-        mkdir -p "$GB_REPO_BASE_DIR"
-        git clone "$GB_REPO_URL" "$GB_REPO_MASTER_DIR"
-    fi
-    _gb_cd_branch "$GB_REPO" "master"
+
+    mkdir -p "$GB_REPO_BASE_DIR"
+    git clone -q "$GB_REPO_URL" "$GB_REPO_MASTER_DIR"
+    cd "$GB_REPO_MASTER_DIR"
+    git checkout -q -b "$GB_MASTER_BRANCH"
+    _gb_start_branch "$GB_REPO" "master"
 }
 
 function _gb_complete {
@@ -266,44 +295,19 @@ function _gb_complete {
         local GB_REPO=${COMP_WORDS[1]}
         local GB_BRANCH=${COMP_WORDS[2]}
         if [ -d "$(_gb_branch_dir "$GB_REPO" "$GB_BRANCH")" ]; then
-            COMPREPLY=($(compgen -W "cd lib merge rm" -- $cur))
+            COMPREPLY=($(compgen -W "finish rm" -- $cur))
         else
             if [ "$GB_BRANCH" != "init" ]; then
-                COMPREPLY=($(compgen -W "new" -- $cur))
+                COMPREPLY=($(compgen -W "start" -- $cur))
             fi
         fi
     fi
 }
 
-function _gb_fix {
-    _gb_env
-    local GB_REPO="$1"
-    local GB_REPO_DIR="$GB_BASE_DIR/$GB_REPO"
-
-    cd "$GB_REPO_DIR/master"
-    if (( $(git status -s | wc -l) )); then
-        echo "ERROR: Uncommitted changes in repo:"
-        git status -s
-        echo ""
-        echo "Commit or remove your changes and then run fix again."
-        return 255
-    fi
-
-    cd "$GB_REPO_DIR"
-    mv master .gb_master
-    cd "$GB_REPO_DIR/.gb_master/"
-    git checkout -b gb_master -t "$GB_DEV_SRC_BRANCH"
-    perl ~nnutter/scripts/fix_broken_symlinks "$GB_REPO_DIR"
-    cd "$GB_REPO_DIR"
-    echo "Now add a 'export GB_DEV=1' to your .bashrc (before you source git-branch.sh) until I've updated the stable version."
-}
-
 function git-branch {
     _gb_env
-    local GB_REPO="$1"
-    local GB_DO_ACTION=1;
+    GB_REPO="$1"
     local GB_ACTION
-    local GB_BRANCH
     case $# in
         0)
             GB_ACTION="help"
@@ -321,7 +325,7 @@ function git-branch {
             ;;
         *)
             echo "ERROR: Too many arguments specified."
-            GB_DO_ACTION=0
+            return 255
             ;;
     esac
 
@@ -330,63 +334,67 @@ function git-branch {
         GB_ACTION="init"
     fi
 
-    if [ "$GB_BRANCH" == "fix" ]; then
-        GB_BRANCH=""
-        GB_ACTION="fix"
-    fi
-
-    if [ "$GB_BRANCH" == "rm" ] || [ "$GB_BRANCH" == "new" ] ||  [ "$GB_BRANCH" == "init" ] || [ "$GB_BRANCH" == "merge" ] || [ "$GB_BRANCH" == "lib" ]; then
-        GB_DO_ACTION=0
+    if [ "$GB_BRANCH" == "rm" ] || [ "$GB_BRANCH" == "start" ] ||  [ "$GB_BRANCH" == "init" ] || [ "$GB_BRANCH" == "finish" ] || [ "$GB_BRANCH" == "lib" ]; then
         echo "ERROR: No branch argument specified."
+        return 255
     fi
 
     for item in $*; do
         if [ "$item" == "--help" ] || [ "$item" == "-h" ]; then
             GB_ACTION="help"
-            GB_DO_ACTION=1
         fi
     done
 
-    if (( $GB_DO_ACTION )); then
-        case $GB_ACTION in
-            help)
-                _gb_help
-                ;;
-            ls)
-                _gb_branches "$GB_REPO"
-                ;;
-            cd)
-                _gb_cd_branch "$GB_REPO" "$GB_BRANCH"
-                ;;
-            rm)
-                _gb_rm_branch "$GB_REPO" "$GB_BRANCH"
-                ;;
-            new)
-                _gb_new_branch "$GB_REPO" "$GB_BRANCH"
-                ;;
-            merge)
-                _gb_merge_branch "$GB_REPO" "$GB_BRANCH"
-                ;;
-            init)
-                local GB_REPO_URL="$GB_BRANCH"
-                _gb_init_repo "$GB_REPO" "$GB_REPO_URL"
-                ;;
-            lib)
-                _gb_lib_path "$GB_REPO" "$GB_BRANCH"
-                ;;
-            fix)
-                _gb_fix "$GB_REPO"
-                ;;
-            *)
-                echo "ERROR: Unrecognized action ($GB_ACTION)."
-                ;;
-        esac
-    fi
+    case $GB_ACTION in
+        help)
+            _gb_help
+        ;;
+        ls)
+            _gb_branches "$GB_REPO"
+        ;;
+        cd)
+            if ! _gb_cd_branch "$GB_REPO" "$GB_BRANCH"; then
+                echo "ERROR: Failed to cd to branch."
+                return 255
+            fi
+        ;;
+        rm)
+            if ! _gb_rm_branch "$GB_REPO" "$GB_BRANCH"; then
+                echo "ERROR: Failed to remove branch."
+                return 255
+            fi
+        ;;
+        start)
+            if ! _gb_start_branch "$GB_REPO" "$GB_BRANCH"; then
+                echo "ERROR: Failed to start branch."
+                return 255
+            fi
+        ;;
+        finish)
+            if ! _gb_finish_branch "$GB_REPO" "$GB_BRANCH"; then
+                echo "ERROR: Failed to finish branch."
+                return 255
+            fi
+        ;;
+        init)
+            local GB_REPO_URL="$GB_BRANCH"
+            if ! _gb_init_repo "$GB_REPO" "$GB_REPO_URL"; then
+                echo "ERROR: Failed to init repo."
+                return 255
+            fi
+        ;;
+        lib)
+            _gb_lib_path "$GB_REPO" "$GB_BRANCH"
+        ;;
+        *)
+            echo "ERROR: Unrecognized action ($GB_ACTION)."
+        ;;
+    esac
 }
 
 function git-track {
     local TRACK_BRANCH="$1"
-    local CURRENT_BRANCH=$(git branch 2> /dev/null | grep ^\* | sed 's/^\* //')
+    local CURRENT_BRANCH=$(_git_current_branch)
     echo -n "Are you sure you wish to replace the current working directory with $TRACK_BRANCH's (y/n)? "
     local response
     read -n 1 response && echo
@@ -394,4 +402,12 @@ function git-track {
         git branch --set-upstream "$CURRENT_BRANCH" "$TRACK_BRANCH"
         git reset --hard "$TRACK_BRANCH"
     fi
+}
+
+function _git_current_branch {
+    git branch | grep ^\* | sed 's/^\* //'
+}
+
+function _git_branch_exists {
+    git show-ref --quiet "$1"
 }
