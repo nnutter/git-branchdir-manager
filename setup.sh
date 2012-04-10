16e9b69f2fac742c40d09d8b79cd94500c9a1e0b
Nathan Nutter <iam@nnutter.com>
2012-04-10 18:03:56 -0500

    add bashrc and setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o errexit
+
+curl -f https://raw.github.com/nnutter/git-branchdir-manager/master/bashrc >> ${HOME}/.bashrc
+mkdir ${HOME}/.bashrc.d
+cd ${HOME}/.bashrc.d
+git clone git://github.com/nnutter/git-branchdir-manager.git ${HOME}/.bashrc.d/git-branchdir-manager
+source ${HOME}/.bashrc.d/git-branchdir-manager/git-branchdir-manager.sh
